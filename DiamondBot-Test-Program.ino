edb41c636a627956a3635594934beb739466aa86
james-mcdougall <jmcdouga@ucsd.edu>
2017-11-27 14:40:19 -0800

    Add files via upload

---
+++ DiamondBot-Test-Program.ino
@@ -0,0 +1,39 @@
+#include <Servo.h>
+#include "DiamondBot.h"
+
+void setup()
+{
+  Serial.begin(9600);
+  buzzer.setup();
+  drive.setup();
+  Servo.setup();
+  pincer.setup();
+  led.setup();
+//  knob.setup();
+  distanceSensor.setup();
+  
+}
+
+void loop()
+{
+  led.turnOn();
+  drive.backward();
+  
+  if(distanceSensor.get_distance() < 10) {
+    drive.stop();
+    Servo.rotate(90);
+    delay(1000);
+    buzzer.playNote(NOTE_A4,300);
+    delay(300);
+    buzzer.playNote(NOTE_B4,300);
+    delay(300);
+    buzzer.playNote(NOTE_C4,300);
+    delay(300);
+    drive.spinLeft();
+    delay(1000);
+    drive.stop();
+    Servo.rotate(0);
+    delay(1000);
+  
+  }        
+}
---
+++ DiamondBot.h
@@ -0,0 +1,37 @@
+#ifndef DIAMONDBOT_H
+#define DIAMONDBOT_H
+
+#include "Gadgetron.h"
+
+
+#define BUZZER_1 5        
+Buzzer buzzer(BUZZER_1);
+
+#define DRIVE_STBY 8        
+#define DRIVE_PWMA 6        
+#define DRIVE_AIN1 10        
+#define DRIVE_AIN2 11        
+#define DRIVE_PWMB 9        
+#define DRIVE_BIN1 12        
+#define DRIVE_BIN2 13        
+Motor drive(DRIVE_STBY,DRIVE_PWMA,DRIVE_AIN1,DRIVE_AIN2,DRIVE_PWMB,DRIVE_BIN1,DRIVE_BIN2);
+
+#define SERVO_DATA 3        
+ServoMotor Servo(SERVO_DATA);
+
+#define PINCER_DATA 4        
+Pincer pincer(PINCER_DATA);
+
+#define LED_CONTROL A1        
+LED led(LED_CONTROL);
+
+#define KNOB_SENSE A2        
+#define KNOB_A A3        
+#define KNOB_B A4        
+//RotaryEncoder knob(KNOB_SENSE,KNOB_A,KNOB_B);
+
+#define DISTANCESENSOR_A A0        
+DistanceSensor distanceSensor(DISTANCESENSOR_A);
+
+
+#endif
